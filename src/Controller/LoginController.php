e934b842fd72eb2bbb2cdc976f5cf307cfcd2d2b
Selmen <selmen.mabrouk@esprit.tn>
2022-03-06 21:47:35 +0100

    init

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+class LoginController extends AbstractController
+{
+    /**
+     * @Route("/connexion", name="app_login")
+     */
+    public function index(AuthenticationUtils $authenticationUtils): Response
+      {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+         // last username entered by the user
+         $lastUsername = $authenticationUtils->getLastUsername();
+
+          return $this->redirectToRoute('accueil', [
+             'last_username' => $lastUsername,
+             'error'         => $error,
+          ]);
+      }
+      /**
+     * @Route("/deconnexion", name="app_logout", methods={"GET"})
+     */
+    public function logout(): void
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
+  }
